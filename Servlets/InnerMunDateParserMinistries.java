d16ed889049db2c9d1749105b75a23128a0f4792
Lazaros Apostolidis <laaposto@gmail.com>
2013-10-01 23:38:30 +0300

    Update InnerMunDateParserMinistries.java

--- Servlets/InnerMunDateParserMinistries.java
+++ Servlets/InnerMunDateParserMinistries.java
@@ -397,7 +397,7 @@ root.addContent(item);
         element.addContent(t);
         item.addContent(element);
 
-         element = new org.jdom.Element("sub");
+        element = new org.jdom.Element("sub");
         t=subjectarray[i];
         element.addContent(t);
         item.addContent(element);
