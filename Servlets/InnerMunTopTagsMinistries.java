b9a3632b163dfc729d32e8b85540c1c6fe4a861c
Lazaros Apostolidis <laaposto@gmail.com>
2013-10-02 00:06:14 +0300

    Update InnerMunTopTagsMinistries.java

--- Servlets/InnerMunTopTagsMinistries.java
+++ Servlets/InnerMunTopTagsMinistries.java
@@ -149,12 +149,6 @@ public class InnerMunTopTagsMinistries extends HttpServlet {
 558,559,560,561,562,563,564,565,566,567,568,569,570,571,572,573,574,575,576,577,578,579,580,581,582,583,584,585,586,587,588,589,590,591,592};
 
 
-
-
-
-//String units;
-
-
  protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -182,8 +176,6 @@ int countglobal=0;
 int orgcount;
 int sum=0;
 
-//String datefrom=request.getParameter("datefrom");
-//String dateto=request.getParameter("dateto");
 
 String mun=request.getParameter("mun");
 String mymun=mun.replaceAll("%20"," ");
