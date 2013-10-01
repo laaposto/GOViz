61b7f80483df1ce3b75d034582bee765bde3d8b8
Lazaros Apostolidis <laaposto@gmail.com>
2013-10-02 00:01:29 +0300

    Update TopTagsParserMunicipalities.java

--- Servlets/TopTagsParserMunicipalities.java
+++ Servlets/TopTagsParserMunicipalities.java
@@ -24,13 +24,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 
-
-
-
-
-
-
-
 public class TopTagsParserMunicipalities extends HttpServlet implements Runnable {
 
 
@@ -39,9 +32,9 @@ Connection con;
 Thread searcher;
 
        public void init(ServletConfig config) throws ServletException {
-    super.init(config);                  // always!
+    super.init(config);                 
     searcher = new Thread(this);
-    searcher.setPriority(Thread.MIN_PRIORITY);  // be a good citizen
+    searcher.setPriority(Thread.MIN_PRIORITY);  
     searcher.start();
 
         try {
@@ -498,7 +491,7 @@ if(countglobal==500){
 
                 }
 
-//res.getWriter().write("OK");
+
 
 
 
